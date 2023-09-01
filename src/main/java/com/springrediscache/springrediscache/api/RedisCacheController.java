a83189d7677f28f8d633ca15243de8547871f26a
Eyuphan Oguz <75530935+eyuphan-oguz@users.noreply.github.com>
2023-09-01 13:19:41 +0300

    first commit

---
+++ src/main/java/com/springrediscache/springrediscache/api/RedisCacheController.java
@@ -0,0 +1,30 @@
+package com.springrediscache.springrediscache.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springrediscache.springrediscache.service.RedisCacheService;
+
+@RestController
+@RequestMapping("/test")
+public class RedisCacheController {
+
+    private int sayac = 0;
+
+    @Autowired
+    private RedisCacheService redisCacheService;
+    
+
+    @GetMapping()
+    public String cacheControl() throws InterruptedException{
+        if(sayac == 5){
+            redisCacheService.clearCache();
+            sayac=0;
+        }
+        sayac++;
+        return redisCacheService.longRunningMethod();
+    }
+
+}
